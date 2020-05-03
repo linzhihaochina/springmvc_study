ba44bbc5ccc95849998f91f2c3d21191d12dd8e2
林志豪A <linzh@jiedaibao.com>
2020-05-03 16:19:50 +0800

    提交作业

--- springmvc_test/src/main/java/com/youngforcoding/annotation/Controller.java
+++ springmvc_test/src/main/java/com/youngforcoding/annotation/Controller.java
@@ -1,13 +1,21 @@
 package com.youngforcoding.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *    
  *  *  
- *  * @Description:  [一句话描述该类的功能]   
+ *  * @Description:  标记Handler类   
  *  * @Author:       linZhiHao  
  *  * @CreateDate:   2020-05-03 11:11   
  *  *    
  *  
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Controller {
+    String value() default "";
 }
